25e6d6f78b2bb78422e4ae30c82b98de81e67d0d
Viktor Pidhornyy <viktor.podgornyy1@gmail.com>
2025-09-19 21:13:58 +0300

    rename project

---
+++ BitsOrchestraTestTask/BitsOrchestraTestTask/DAL/UserDAL.cs
@@ -0,0 +1,144 @@
+﻿using BitsOrchestraTestTask.BL;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BitsOrchestraTestTask.DAL
+{
+	public class UserDAL
+	{
+		private ILogger<UserDAL> _logger;
+		private IConfiguration _configuration;
+		public UserDAL(ILogger<UserDAL> logger, IConfiguration configuration)
+		{
+			_logger = logger;
+			_configuration = configuration;
+		}
+
+		public async Task<Result<List<User>>> GetAllUsers()
+		{
+			await using (SqlConnection connection = new SqlConnection(_configuration["ConnectionStrings:MainConnection"]))
+			{
+				connection.Open();
+				string sql = "SELECT UserId, UserName, DateOfBirth, Married, Phone, Salary FROM UserInfo";
+				SqlCommand command = new SqlCommand(sql, connection);
+				command.CommandType = CommandType.Text;
+				await using (SqlDataReader reader = command.ExecuteReader())
+				{
+					Result<List<User>> result = new Result<List<User>>();
+					result.Data = new List<User>();
+					while (reader.Read())
+					{
+						User user = new User
+						{
+							Id = reader.GetInt32(reader.GetOrdinal("UserId")),
+							Name = reader.GetString(reader.GetOrdinal("UserName")),
+							DateOfBirth = reader.GetDateTime(reader.GetOrdinal("DateOfBirth")),
+							Married = reader.GetBoolean(reader.GetOrdinal("Married")),
+							Phone = reader.GetString(reader.GetOrdinal("Phone")),
+							Salary = reader.GetDecimal(reader.GetOrdinal("Salary"))
+						};
+						result.Data.Add(user);
+					}
+					result.ErrorCode = 0;
+					return result;
+				}
+			}
+		}
+
+		public async Task<Result> DeleteUser(int id)
+		{
+			await using (SqlConnection connection = new SqlConnection(_configuration["ConnectionStrings:MainConnection"]))
+			{
+				Result result = new Result();
+				connection.Open();
+				string sql = "DELETE FROM UserInfo Where UserId=@id";
+				SqlCommand command = new SqlCommand(sql, connection);
+				command.CommandType = CommandType.Text;
+				command.Parameters.AddWithValue("@id", id);
+				int rowsAffected = await command.ExecuteNonQueryAsync();
+				if (rowsAffected == 0)
+				{
+					result.ErrorCode = 404;
+					result.ErrorMessage = "User not found";
+				}
+				result.ErrorCode = 0;
+				return result;
+			}
+		}
+
+		public async Task<Result> CreateUsers(List<User> users)
+		{
+			Result result = new Result();
+			await using (SqlConnection connection = new SqlConnection(_configuration["ConnectionStrings:MainConnection"]))
+			{
+				connection.Open();
+				SqlTransaction transaction = connection.BeginTransaction();
+				SqlCommand command = connection.CreateCommand();
+				command.Connection = connection;
+				command.Transaction = transaction;
+				try
+				{
+					string sql = "INSERT INTO UserInfo (UserName, DateOfBirth, Married, Phone, Salary)" +
+						"VALUES(@name, @dateOfBirth, @married, @phone, @salary)";
+					for (int i = 0; i < users.Count; i++)
+					{
+						command.CommandText = sql;
+						command.Parameters.AddWithValue("@name", users[i].Name);
+						command.Parameters.AddWithValue("@dateOfBirth", users[i].DateOfBirth);
+						command.Parameters.AddWithValue("@married", users[i].Married);
+						command.Parameters.AddWithValue("@phone", users[i].Phone);
+						command.Parameters.AddWithValue("@salary", users[i].Salary);
+						int rowsAffected = await command.ExecuteNonQueryAsync();
+						if (rowsAffected <= 0)
+						{
+							transaction.Rollback();
+							result.ErrorCode = 500;
+							result.ErrorMessage = $"Failed to insert user int DB. User {i}";
+							_logger.LogError(result.ErrorMessage);
+							return result;
+						}
+						command.Parameters.Clear();
+					}
+					await transaction.CommitAsync();
+				}
+				catch (Exception ex)
+				{
+					transaction.Rollback();
+					_logger.LogError(ex, "Error creating users");
+					result.ErrorCode = ex.HResult;
+					result.ErrorMessage = ex.Message;
+					return result;
+				}
+			}
+			result.ErrorCode = 0;
+			return result;
+		}
+
+		public async Task<Result> UpdateUser(User user)
+		{
+			Result result = new Result();
+			await using (SqlConnection connection = new SqlConnection(_configuration["ConnectionStrings:MainConnection"]))
+			{
+				connection.Open();
+				string sql = "UPDATE UserInfo SET UserName=@name, DateOfBirth=@dateOfBirth," +
+					"Married=@married, Phone=@phone, Salary=@salary Where UserId=@id";
+				SqlCommand command = new SqlCommand(sql, connection);
+				command.CommandType = CommandType.Text;
+				command.Parameters.AddWithValue("@id", user.Id);
+				command.Parameters.AddWithValue("@name", user.Name);
+				command.Parameters.AddWithValue("@dateOfBirth", user.DateOfBirth);
+				command.Parameters.AddWithValue("@married", user.Married);
+				command.Parameters.AddWithValue("@phone", user.Phone);
+				command.Parameters.AddWithValue("@salary", user.Salary);
+				int rowsAffected = await command.ExecuteNonQueryAsync();
+				if (rowsAffected == 0)
+				{
+					result.ErrorCode = 404;
+					result.ErrorMessage = "User not updated";
+				}
+				result.ErrorCode = 0;
+				return result;
+			}
+		}
+	}
+}
---
+++ BitsOrchestraTestTask/BitsOrchestraTestTask/wwwroot/js/updateUser.js
@@ -0,0 +1,70 @@
+﻿function EditUser(button) {
+    const row = button.closest('tr');
+    let field;
+    let value;
+    row.querySelectorAll('.editable').forEach(cell => {
+        field = cell.getAttribute('data-field');
+        value = cell.textContent.trim();
+        switch (field)
+        {
+            case 'DateOfBirth':
+                cell.innerHTML = `<input type="date" name="${field}" value="${value}" />`;
+                break;
+            case 'Married':
+                const isChecked = value.toLowerCase() === "true";
+                if (isChecked) {
+                    cell.innerHTML = `<input type="checkbox" name="${field}" isChecked='checked' checked/>`;
+                }
+                else
+                {
+                    cell.innerHTML = `<input type="checkbox" name="${field}" isChecked='' />`;
+                }
+                break;
+            case 'Salary':
+                cell.innerHTML = `<input type="number" min=0 step=0.01 name="${field}" value="${value}" />`;
+                break;
+            default:
+                cell.innerHTML = `<input type="text" name="${field}" value="${value}"/>`;
+                break;
+        }
+
+    });
+
+    // Toggle buttons
+    row.querySelector('.editBtn').style.display = 'none';
+    row.querySelector('.saveBtn').style.display = 'block';
+}
+
+function SaveUser(button) {
+    const row = button.closest('tr');
+    const id = row.getAttribute('data-id');
+    const inputs = row.querySelectorAll('input');
+    const data = { Id: id };
+    const marriedCheckbox = row.querySelector('input[name="Married"]');
+
+    inputs.forEach(input => {
+        data.Married = marriedCheckbox.checked;
+        data[input.name] = input.value;
+    });
+
+    let Http = new XMLHttpRequest();
+    let url = `/Home/UpdateUser`
+    Http.open("POST", url);
+    Http.setRequestHeader("Content-Type", "application/json; charset=utf-8")
+    Http.send(JSON.stringify(data));
+
+    Http.onreadystatechange = function () {
+        if (Http.readyState === 4) {
+            if (Http.status === 200) {
+                window.location.href = '/Home/GetAllUsers';
+                row.querySelector('.editBtn').style.display = 'block';
+                row.querySelector('.saveBtn').style.display = 'none';
+            }
+            else
+            {
+                console.log(Http.responseText);
+                window.location.href = '/Home/GetErrorPage';
+            }
+        }
+    };
+}
